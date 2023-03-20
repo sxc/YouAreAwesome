fba6dc38c1ba4814539512fc2ea2567e440e310d
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-20 19:17:51 +0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Koharu on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
